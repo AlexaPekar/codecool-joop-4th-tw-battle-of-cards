68bdbc07d819182f86a0f0c06218c8403d4971cc
AlexaPekar <missdreambelle@gmail.com>
2018-02-19 14:34:37 +0100

    Create RoundImpl class to implement Round interface

---
+++ src/main/java/com/codecool/cardgame/api/RoundImpl.java
@@ -0,0 +1,51 @@
+package com.codecool.cardgame.api;
+
+public class RoundImpl implements Round {
+
+    private Card player1Card;
+    private Card player2Card;
+    private String chosenAttribute;
+
+    public RoundImpl() {
+    }
+
+    @Override
+    public Card getPlayer1Card() {
+        return player1Card;
+    }
+
+    @Override
+    public Card getPlayer2Card() {
+        return player2Card;
+    }
+
+    @Override
+    public String getChosenAttribute() {
+        return chosenAttribute;
+    }
+
+    @Override
+    public void setPlayer1Card(Card card) {
+        player1Card = card;
+    }
+
+    @Override
+    public void setPlayer2Card(Card card) {
+        player2Card = card;
+    }
+
+    @Override
+    public void setChosenAttribute(String attribute) {
+        chosenAttribute = attribute;
+    }
+
+    @Override
+    public Player getWinner() {
+        return null;
+    }
+
+    @Override
+    public void decreaseHP(Player player , int damage) {
+
+    }
+}
