bee176c3f731148e17f1d37aae88ed9a60572592
AlexaPekar <missdreambelle@gmail.com>
2018-02-19 14:21:48 +0100

    Create Round interface containing methods for RoundImpl

---
+++ src/main/java/com/codecool/cardgame/api/Round.java
@@ -0,0 +1,12 @@
+package com.codecool.cardgame.api;
+
+public interface Round {
+
+    Card getPlayer1Card();
+    Card getPlayer2Card();
+    void setPlayer1Card();
+    void setPlayer2Card();
+    Player getWinner();
+    void decreaseHP();
+    
+}
