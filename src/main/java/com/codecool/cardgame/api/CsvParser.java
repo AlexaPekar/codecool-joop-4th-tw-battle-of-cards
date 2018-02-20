adbda80b984f4467b042d1842c5a757bb082ac48
GaborMartin <gabormartin94@gmail.com>
2018-02-20 12:08:39 +0100

    Modified CsvParser to handle both Card types

--- src/main/java/com/codecool/cardgame/api/CsvParser.java
+++ src/main/java/com/codecool/cardgame/api/CsvParser.java
@@ -14,9 +14,13 @@ public class CsvParser {
     public List<Card> getCardsFromCsv(String filepath) {
         try (BufferedReader br = new BufferedReader(new FileReader("../data/" + csvFile))) {
             while ((line = br.readLine()) != null) {
-                String[] wizard = line.split(",");
-                Card card = new Card(wizard[0],Integer.parseInt(wizard[1]),Integer.parseInt(wizard[2]),Integer.parseInt(wizard[3]));
-                deck.add(card);
+                String[] card = line.split(",");
+                if (card[0].equals("Fighter")) {
+                    FighterCard fighterCard = new FighterCard(card[1],Integer.parseInt(card[2]),Integer.parseInt(card[3]),Integer.parseInt(card[4]));
+                    deck.add(fighterCard);
+                }
+                SpellCard spellCard = new SpellCard(card[1], card[2], card[3]);
+                deck.add(spellCard);
             }
         } catch (IOException e) {
             System.out.println(" Exception error. File not found.");
