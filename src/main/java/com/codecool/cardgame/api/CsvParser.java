e9d463eed2014078c7796a1a781fcb5969bb6694
GaborMartin <gabormartin94@gmail.com>
2018-02-20 13:12:50 +0100

    Modified exception handling in getCardsFromCsv method, fixed issues in constructors

--- src/main/java/com/codecool/cardgame/api/CsvParser.java
+++ src/main/java/com/codecool/cardgame/api/CsvParser.java
@@ -11,21 +11,21 @@ public class CsvParser {
         String csvFile;
         List<Card> deck = new ArrayList<Card>();
 
-    public List<Card> getCardsFromCsv(String filepath) {
-        try (BufferedReader br = new BufferedReader(new FileReader("../data/" + csvFile))) {
+    public List<Card> getCardsFromCsv(String filepath) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("../data/" + csvFile));
+            if (br == null) {
+                throw new IOException();
+            }
             while ((line = br.readLine()) != null) {
                 String[] card = line.split(",");
-                if (card[0].equals("Fighter")) {
-                    FighterCard fighterCard = new FighterCard(card[1],Integer.parseInt(card[2]),Integer.parseInt(card[3]),Integer.parseInt(card[4]));
+                if (card[1].equals("Fighter")) {
+                    FighterCard fighterCard = new FighterCard(card[0], card[1], Integer.parseInt(card[3]), Integer.parseInt(card[4]), Integer.parseInt(card[5]));
                     deck.add(fighterCard);
+                } else {
+                    SpellCard spellCard = new SpellCard(card[1], card[2], card[3]);
+                    deck.add(spellCard);
                 }
-                SpellCard spellCard = new SpellCard(card[1], card[2], card[3]);
-                deck.add(spellCard);
             }
-        } catch (IOException e) {
-            System.out.println(" Exception error. File not found.");
-            e.printStackTrace();
-        }
         return deck;
     }
 }
