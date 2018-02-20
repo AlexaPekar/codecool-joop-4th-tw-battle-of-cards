b5afa9706d379da18a805982c3ab21dcfbd2d98c
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-02-20 13:07:05 +0100

    type field added

--- src/main/java/com/codecool/cardgame/api/Card.java
+++ src/main/java/com/codecool/cardgame/api/Card.java
@@ -2,13 +2,18 @@ package com.codecool.cardgame.api;
 
 public abstract class Card{
     private String name;
+    private String type;
 
-    public Card(String name) {
+    public Card(String name,String type) {
         this.name = name;
+        this.type = type;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getType() {
+        return type;
+    }
 }
--- src/main/java/com/codecool/cardgame/api/FighterCard.java
+++ src/main/java/com/codecool/cardgame/api/FighterCard.java
@@ -5,8 +5,8 @@ public class FighterCard extends Card {
     int damage;
     int defense;
 
-    public FighterCard(String name, int intelligence, int damage, int defense) {
-        super(name);
+    public FighterCard(String name,String type, int intelligence, int damage, int defense) {
+        super(name,type);
     }
 
     public int getIntelligence() {
@@ -25,13 +25,17 @@ public class FighterCard extends Card {
         this.intelligence = intelligence;
     }
 
-    public void setDamage(int damage) {
-        this.damage = damage;
-    }
+    public void setDamage(int damage) { this.damage = damage; }
 
     public void setDefense(int defense) {
         this.defense = defense;
     }
+
+    public void increaseDamage(int damage) { this.damage += damage; }
+
+    public void increaseIntelligence(int intelligence) { this.intelligence += intelligence; }
+
+    public void increaseDefense(int defense) { this.defense += defense; }
 }
 
 
