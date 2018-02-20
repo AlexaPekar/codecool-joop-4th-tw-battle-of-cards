04580b2509fe7d436f577afd03684b9d7ad5c415
GaborMartin <gabormartin94@gmail.com>
2018-02-20 11:26:40 +0100

    Modified class Card to abstract, modified constructor

--- src/main/java/com/codecool/cardgame/api/Card.java
+++ src/main/java/com/codecool/cardgame/api/Card.java
@@ -1,31 +1,14 @@
 package com.codecool.cardgame.api;
 
-public class Card{
+public abstract class Card{
     private String name;
-    private int intelligence;
-    private int damage;
-    private int defense;
 
-    public Card(String name, int intelligence, int damage, int defense) {
+    public Card(String name) {
         this.name = name;
-        this.intelligence = intelligence;
-        this.damage = damage;
-        this.defense = defense;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getIntelligence() {
-        return intelligence;
-    }
-
-    public int getDamage() {
-        return damage;
-    }
-
-    public int getDefense() {
-        return defense;
-    }
 }
