1eab9c35557d638ad37480ad79153d1945b39052
AlexaPekar <missdreambelle@gmail.com>
2018-02-20 14:41:23 +0100

    Create exception classes

---
+++ src/main/java/com/codecool/cardgame/api/exception/CardException.java
@@ -0,0 +1,10 @@
+package com.codecool.cardgame.api.exception;
+
+import com.codecool.cardgame.api.Card;
+
+public class CardException extends Exception {
+
+    CardException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/codecool/cardgame/api/exception/NoManaException.java
@@ -0,0 +1,8 @@
+package com.codecool.cardgame.api.exception;
+
+public class NoManaException extends CardException {
+
+    public NoManaException(String message) {
+        super(message);
+    }
+}
