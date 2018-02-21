05194b92b9dd88034726dd788dc88db1afb2ff81
AlexaPekar <missdreambelle@gmail.com>
2018-02-21 11:30:28 +0100

    Fix wrong indexes in CsvParser class

--- src/main/java/com/codecool/cardgame/api/CsvParser.java
+++ src/main/java/com/codecool/cardgame/api/CsvParser.java
@@ -15,7 +15,7 @@ public class CsvParser {
             while ((line = br.readLine()) != null) {
                 String[] card = line.split(",");
                 if (card[1].equals("Fighter")) {
-                    FighterCard fighterCard = new FighterCard(card[0], card[1], Integer.parseInt(card[3]), Integer.parseInt(card[4]), Integer.parseInt(card[5]));
+                    FighterCard fighterCard = new FighterCard(card[0], card[1], Integer.parseInt(card[2]), Integer.parseInt(card[3]), Integer.parseInt(card[4]));
                     deck.add(fighterCard);
                 } else {
                     SpellCard spellCard = new SpellCard(card[0], card[1], card[2], Integer.parseInt(card[3]));
